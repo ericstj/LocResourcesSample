4f1c84c79b5226c8a269f7cd4a7454378a7af438
Eric StJohn <ericstj@microsoft.com>
2019-05-28 10:39:12 -0700

    Add simple sample that has a CustomControl with localized properties

---
+++ LocTest/CustomControl.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Design;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocTest
+{
+    public partial class CustomControl : Control
+    {
+        public CustomControl()
+        {
+            InitializeComponent();
+            ByteArrayProp = new byte[8];
+            StreamProp = new MemoryStream(new byte[8]);
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+
+        [Category("Loc")]
+        [Localizable(true)]
+        public string StringProp { get; set; }
+
+        [Category("Loc")]
+        [Localizable(true)]
+        public int IntProp { get; set; }
+
+        [Category("Loc")]
+        [Localizable(true)]
+        public bool BoolProp { get; set; }
+
+        [Category("Loc")]
+        [Localizable(true)]
+        public char CharProp { get; set; }
+
+        [Category("Loc")]
+        [Localizable(true)]
+        public byte ByteProp { get; set; }
+
+        [Category("Loc")]
+        [Localizable(true)]
+        public sbyte SByteProp { get; set; }
+
+        [Category("Loc")]
+        [Localizable(true)]
+        public short ShortProp { get; set; }
+
+        [Category("Loc")]
+        [Localizable(true)]
+        public long LongProp { get; set; }
+
+        [Category("Loc")]
+        [Localizable(true)]
+        public ushort UShortProp { get; set; }
+
+        [Category("Loc")]
+        [Localizable(true)]
+        public uint UIntProp { get; set; }
+
+        [Category("Loc")]
+        [Localizable(true)]
+        public ulong ULongProp { get; set; }
+
+        [Category("Loc")]
+        [Localizable(true)]
+        public float FloatProp { get; set; }
+
+        [Category("Loc")]
+        [Localizable(true)]
+        public double DoubleProp { get; set; }
+
+        [Category("Loc")]
+        [Localizable(true)]
+        public decimal DecimalProp { get; set; }
+
+
+        [Category("Loc")]
+        [Localizable(true)]
+        public DateTime DateTimeProp { get; set; }
+
+
+        [Category("Loc")]
+        [Localizable(true)]
+        public TimeSpan TimeSpanProp { get; set; }
+
+        [Category("Loc")]
+        [Localizable(true)]
+        [Editor(typeof(System.ComponentModel.Design.ArrayEditor), typeof(System.Drawing.Design.UITypeEditor))]
+        public byte[] ByteArrayProp { get; set; }
+
+        [Category("Loc")]
+        [Localizable(true)]
+        [Editor(typeof(BinaryEditor), typeof(UITypeEditor))]
+        public Stream StreamProp { get; set; }
+
+
+    }
+}
---
+++ LocTest/Form1.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace LocTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.customControl1 = new LocTest.CustomControl();
+            this.SuspendLayout();
+            // 
+            // customControl1
+            // 
+            resources.ApplyResources(this.customControl1, "customControl1");
+            this.customControl1.Name = "customControl1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.customControl1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CustomControl customControl1;
+    }
+}
+
